32203b1e51bc370f13f8c41b82aa2adc9c8f8c5a
Gabby AK <gkadiga1@gmail.com>
2026-09-15 15:39:07 +0200

    webServer GET - maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
         var app = Javalin.create(config -> {
             config.routes.get("/", ctx -> ctx.result("Hej verden"));
+            config.routes.get("/hello", ctx -> ctx.result("Hello!"));
+            config.routes.get("/about", ctx -> ctx.result("Hej Webserver"));
+            config.routes.get("/contact", ctx -> ctx.result("Kontakt webserver"));
         }).start(7070);
     }
 
